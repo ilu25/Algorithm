98f155c4de96111fc5d66f95d830067903d55f61
조세영 <93782168+ilu25@users.noreply.github.com>
2024-04-09 17:35:10 +0900

    [Silver II] Title: 종이의 개수, Time: 1020 ms, Memory: 313808 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static int[][] board;
+	public static int MINUS = 0;
+	public static int ZERO = 0;
+	public static int PLUS = 0;
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		StringTokenizer st;
+ 
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+ 
+		partition(0, 0, N);
+ 
+		System.out.println(MINUS);
+		System.out.println(ZERO);
+		System.out.println(PLUS);
+ 
+	}
+ 
+	public static void partition(int row, int col, int size) {
+		
+		if (colorCheck(row, col, size)) {
+			// 영역이 같은 색이면 1 증가
+			if(board[row][col] == -1) { 
+				MINUS++;
+			}
+			else if(board[row][col] == 0) {
+				ZERO++;
+			}
+			else {
+				PLUS++;
+			}
+			return;
+		}
+		
+		// 3x3 영역으로 분할 -> 추후 영역에 다른 색이 있으면 분할
+		int newSize = size / 3;
+		
+		partition(row, col, newSize);								// 왼쪽 위
+		partition(row, col + newSize, newSize);						// 중앙 위
+		partition(row, col + 2 * newSize, newSize);					// 오른쪽 위
+		
+		partition(row + newSize, col, newSize);						// 왼쪽 중간
+		partition(row + newSize, col + newSize, newSize);			// 중앙 중간
+		partition(row + newSize, col + 2 * newSize, newSize);		// 오른쪽 중간
+		
+		partition(row + 2 * newSize, col, newSize);					// 왼쪽 아래
+		partition(row + 2 * newSize, col + newSize, newSize);		// 중앙 아래
+		partition(row + 2 * newSize, col + 2 * newSize, newSize);	// 오른쪽 아래
+ 
+	}
+ 
+	// 해당 영역이 같은 색인지 검사하는 메소드
+	public static boolean colorCheck(int row, int col, int size) {
+		int color = board[row][col];
+ 
+		// 각 블럭의 시작점(row, col)부터 블럭의 끝(row + size, col + size)까지 검사
+		for (int i = row; i < row + size; i++) {
+			for (int j = col; j < col + size; j++) {
+				if (color != board[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+ 
+}
