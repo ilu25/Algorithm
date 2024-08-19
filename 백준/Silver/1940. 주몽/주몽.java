2af061d1a4899b0f004767c3c1da6efd1a1c928d
조세영 <93782168+ilu25@users.noreply.github.com>
2024-08-19 23:40:11 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,21 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = sc.nextInt();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        // 정렬
+        Arrays.sort(arr);
+
+        // 투 포인터
         int count = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] + arr[j] == M) {
-                    count++;
-                    break;
-                }
+        int start = 0;
+        int end = N - 1;
+        while (start < end) {
+            if (arr[start] + arr[end] == M) {
+                count++;
+                start++;
+                end--;
+            }
+            else if (arr[start] + arr[end] < M) {
+                start++;
+            }
+            else {
+                end--;
             }
         }
         System.out.println(count);
