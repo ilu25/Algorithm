3c39ee22a6ee0bd785577b9df8b428b56de8926c
조세영 <93782168+ilu25@users.noreply.github.com>
2024-12-19 19:16:38 +0900

    [Bronze I] Title: 이항 계수 1, Time: 168 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,29 +1,20 @@
 import java.util.Scanner;
 
 class Main {
-    static int N, K;
-    static int[][] D;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
-        D = new int[N + 1][N + 1];
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
-        // 초기화
-        for (int i = 1; i <= N; i++) {
-            D[i][0] = 1;
-            D[i][1] = i;
-            D[i][i] = 1;    // 이것 때문에 배열 N x N
-        }
+        int numerator = factorial(N);
+        int denominator = factorial(N - K) * factorial(K);
+        int combination = numerator / denominator;
 
-        // 점화식으로 배열 완성
-        for (int i = 2; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                D[i][j] = D[i - 1][j - 1] + D[i - 1][j];
-            }
-        }
+        System.out.println(combination);
+    }
 
-        System.out.println(D[N][K]);
+    private static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
     }
 }
