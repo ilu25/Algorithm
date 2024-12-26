60312764586301d7a75d788cf792b21f9d92518f
조세영 <93782168+ilu25@users.noreply.github.com>
2024-12-26 23:34:50 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(" ".repeat(N - i)).append("*".repeat(2*i - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
