af38058e5dd259942305570e8192c828c25062f4
조세영 <93782168+ilu25@users.noreply.github.com>
2025-01-11 00:08:45 +0900

    [Silver II] Title: 쇠막대기, Time: 80 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,33 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
-// 11:09 ~ 11:51
+// 다른 사람 코드 ver.
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] input = br.readLine().toCharArray();
-        Stack<Character> stack = new Stack<>();
-
         int ans = 0;
-        int numberOfRemainingSticks = 0;
-        for (char c : input) {
-            if (c == '(') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    numberOfRemainingSticks++;
-                }
+        int openCnt = 0;
+
+        for (int i = 0 ; i < input.length ; i++) {
+            if (input[i] == '(') {
+                openCnt++;
             }
-            else if (c == ')') {
-                if (stack.peek() == ')') {
-                    numberOfRemainingSticks--;
+            else {
+                openCnt--;
+                if (input[i - 1] == ')') {
                     ans++;
                 }
                 else {
-                    ans += numberOfRemainingSticks;
+                    ans += openCnt;
                 }
             }
-            stack.push(c);
         }
         System.out.println(ans);
     }
