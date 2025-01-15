8cd0e6b4ed65f49536216f423aec4cba304527b3
조세영 <93782168+ilu25@users.noreply.github.com>
2025-01-15 22:07:25 +0900

    [Silver V] Title: 나이순 정렬, Time: 332 ms, Memory: 44860 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<String>[] members = new ArrayList[201];
+        StringBuilder[] members = new StringBuilder[201];
+
+        for (int i = 1; i < members.length; i++) {
+            members[i] = new StringBuilder();
+        }
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
-
-            if (members[age] == null) {
-                members[age] = new ArrayList<>();
-            }
-            members[age].add(st.nextToken());
+            String name = st.nextToken();
+            members[age].append(age).append(" ").append(name).append("\n");
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < members.length; i++) {
-            if (members[i] != null) {
-                while (!members[i].isEmpty()) {
-                    sb.append(i).append(" ").append(members[i].remove(0)).append("\n");
-                }
-            }
+            sb.append(members[i]);
         }
         System.out.println(sb);
     }
