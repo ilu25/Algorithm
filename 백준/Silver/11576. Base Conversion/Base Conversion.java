e2f5683e91673ab1e26c210f5c8c2e108f18ae9c
조세영 <93782168+ilu25@users.noreply.github.com>
2025-02-10 23:07:25 +0900

    [Silver V] Title: Base Conversion, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.java
+++ 백준/Silver/11576. Base Conversion/Base Conversion.java
@@ -1,14 +1,11 @@
 import java.io.*;
 import java.util.*;
-
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
@@ -19,18 +16,16 @@ class Main {
             num += (int) (a * Math.pow(A, m - 1 - i));
         }
 
-        // 10진법 -> B진법
+        // 10진법 -> B진법 (스택 이용)
         StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         while (num > 0) {
-            list.add(num % B);
+            stack.add(num % B);
             num /= B;
         }
-        for (int i = list.size() - 1; i > 0; i--) {
-            sb.append(list.get(i)).append(" ");
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop()).append(" ");
         }
-        sb.append(list.get(0));
-
         System.out.println(sb);
     }
 }
