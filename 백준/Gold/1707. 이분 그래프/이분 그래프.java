3d8136dbf264b850893561028bf6b699ed6fb69c
조세영 <93782168+ilu25@users.noreply.github.com>
2025-02-22 23:50:50 +0900

    [Gold IV] Title: 이분 그래프, Time: 936 ms, Memory: 284132 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,70 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static ArrayList<Integer>[] A;
-    static int[] check; // 집합 0, 1
+class Main {
+    static List<Integer>[] list;
     static boolean[] visited;
-    static boolean isEven;
+    static int[] group;    // 두 집합
+    static boolean isSame = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        for (int i = 0; i < testCase; i++) {
-            String[] s = br.readLine().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
+        int K = Integer.parseInt(br.readLine());
 
-            A = new ArrayList[V + 1];
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            list = new ArrayList[V + 1];
             visited = new boolean[V + 1];
-            check = new int[V + 1];
-            isEven = true;
-
-            // 에지 데이터 저장
             for (int j = 1; j <= V; j++) {
-                A[j] = new ArrayList<>();
+                list[j] = new ArrayList<>();
             }
+            group = new int[V + 1];
+
             for (int j = 0; j < E; j++) {
-                s = br.readLine().split(" ");
-                int start = Integer.parseInt(s[0]);
-                int end = Integer.parseInt(s[1]);
-                A[start].add(end);
-                A[end].add(start);
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                list[u].add(v);
+                list[v].add(u);
             }
 
-            // 모든 노드에서 DFS 실행
             for (int j = 1; j <= V; j++) {
-                if (isEven) {
-                    DFS(j);
-                }
-                else {
-                    break;
-                }
+                dfs(j);
             }
-
-            if (isEven) {
-                System.out.println("YES");
-            }
-            else {
-                System.out.println("NO");
-            }
+            sb.append(isSame ? "NO" : "YES").append('\n');
+            isSame = false;
         }
+        System.out.println(sb);
     }
 
-    private static void DFS(int v) {
-        visited[v] = true;
+    static void dfs(int i) {
+        visited[i] = true;
 
-        // 인접 리스트로 받아서 v와 연결된 모든 노드 탐색
-        for (int i : A[v]) {
-            if (!visited[i]) {
-                // 바로 직전에 있는 노드와 다른 집합으로 분류
-                check[i] = (check[v] + 1) % 2;  // 0 또는 1
-                DFS(i);
+        for (int v : list[i]) {
+            if (!visited[v]) {
+                group[v] = (group[i] + 1) % 2;
+                dfs(v);
             }
-            else if (check[v] == check[i]) {
-                isEven = false;
+            else if (group[i] == group[v]) {
+                isSame = true;
+                break;
             }
         }
     }
