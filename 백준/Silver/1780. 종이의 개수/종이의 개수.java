4d9ed33a149c51e56978862140b5419a6be1429d
조세영 <93782168+ilu25@users.noreply.github.com>
2025-03-18 16:54:20 +0900

    [Silver II] Title: 종이의 개수, Time: 904 ms, Memory: 319528 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,78 +1,67 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static int[][] board;
-	public static int MINUS = 0;
-	public static int ZERO = 0;
-	public static int PLUS = 0;
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		StringTokenizer st;
- 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
- 
-		partition(0, 0, N);
- 
-		System.out.println(MINUS);
-		System.out.println(ZERO);
-		System.out.println(PLUS);
- 
-	}
- 
-	public static void partition(int row, int col, int size) {
-		
-		if (colorCheck(row, col, size)) {
-			// 영역이 같은 색이면 1 증가
-			if(board[row][col] == -1) { 
-				MINUS++;
-			}
-			else if(board[row][col] == 0) {
-				ZERO++;
-			}
-			else {
-				PLUS++;
-			}
-			return;
-		}
-		
-		// 3x3 영역으로 분할 -> 추후 영역에 다른 색이 있으면 분할
-		int newSize = size / 3;
-		
-		for (int i = row; i < row + size; i += newSize) {
-			for (int j = col; j < col + size; j += newSize) {
-				partition(i, j, newSize);
-			}
-		}
- 
-	}
- 
-	// 해당 영역이 같은 색인지 검사하는 메소드
-	public static boolean colorCheck(int row, int col, int size) {
-		int color = board[row][col];
- 
-		// 각 블럭의 시작점(row, col)부터 블럭의 끝(row + size, col + size)까지 검사
-		for (int i = row; i < row + size; i++) {
-			for (int j = col; j < col + size; j++) {
-				if (color != board[i][j]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
- 
+    static int[][] papers;
+    static int minus = 0;
+    static int zero = 0;
+    static int plus = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        papers = new int[N][N];
+
+        // 종이 입력 받기
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < input.length; j++) {
+                papers[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        divide(0, 0, N);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(minus).append("\n");
+        sb.append(zero).append("\n");
+        sb.append(plus);
+        System.out.print(sb);
+    }
+
+    static void divide(int x, int y, int N) {
+        // 전체 같은 색이면 +1
+        int color = papers[x][y];
+        if (isSame(x, y, N)) {
+            if (color == -1) {
+                minus++;
+            }
+            else if (color == 0) {
+                zero++;
+            }
+            else {
+                plus++;
+            }
+            return;
+        }
+
+        // N/3 x N/3 사이즈로 9번 반복
+        int newN = N / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                divide(x + newN * i, y + newN * j, newN);
+            }
+        }
+    }
+
+    static boolean isSame(int x, int y, int N) {
+        int color = papers[x][y];
+        for (int i = x; i < x+N; i++) {
+            for (int j = y; j < y+N; j++) {
+                if (color != papers[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
