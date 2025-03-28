7b5e8a4eee17615dfe04b275deba48b625826b37
조세영 <93782168+ilu25@users.noreply.github.com>
2025-03-28 20:55:09 +0900

    [level 3] Title: 단어 변환, Time: 0.48 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,24 +3,17 @@ import java.util.*;
 class Solution {
     static int answer = 0;
     static boolean[] visited;
-    static int[] steps;
     
     // 6:12 ~ 
     public int solution(String begin, String target, String[] words) {
         visited = new boolean[words.length];
-        steps = new int[words.length];
-        
-        int targetIdx = Arrays.asList(words).indexOf(target);
-        DFS(begin, target, words, 1);
-        
-        if (targetIdx != -1) {
-            answer = steps[targetIdx];
-        }
+        DFS(begin, target, words, 0);
         return answer;
     }
     
     static void DFS(String current, String target, String[] words, int step) {
         if (current.equals(target)) {
+            answer = step; 
             return;
         }
         
@@ -29,12 +22,6 @@ class Solution {
                 // 글자가 하나만 다른지 확인
                 if (canVisit(current, words[i])) {
                     visited[i] = true;
-                    if (steps[i] == 0) {
-                        steps[i] = step;
-                    }
-                    else {
-                        steps[i] = Math.min(steps[i], step);
-                    }
                     DFS(words[i], target, words, step + 1);
                     visited[i] = false;
                 }
