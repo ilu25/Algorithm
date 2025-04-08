fe34f06095c5dd70e27c5c025261052f88a45af9
조세영 <93782168+ilu25@users.noreply.github.com>
2025-04-08 13:53:03 +0900

    [Gold IV] Title: 전화번호 목록, Time: 356 ms, Memory: 112688 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,39 +1,75 @@
 import java.io.*;
 import java.util.*;
 
+class TrieNode {
+    TrieNode[] children = new TrieNode[10];
+    boolean isEndOfNumber = false;
+}
+
+class Trie {
+    private final TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    public boolean insert(String number) {
+        TrieNode node = root;
+        boolean isPrefix = false;
+        for (char c : number.toCharArray()) {
+            int index = c - '0';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            else {
+                // 현재 노드가 이미 끝나는 전화번호이면 (이미 접두어 존재)
+                if (node.children[index].isEndOfNumber) {
+                    isPrefix = true;
+                }
+            }
+            node = node.children[index];
+        }
+        node.isEndOfNumber = true;
+
+        for (TrieNode child : node.children) {
+            // 마지막 노드가 자식을 갖는다면 (현재의 전화번호가 다른 전화번호의 접두어)
+            if (child != null) {
+                isPrefix = true;
+                break;
+            }
+        }
+        return isPrefix;
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            String[] phone_numbers = new String[N];
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            List<String> phoneNumbers = new ArrayList<>();
+            Trie trie = new Trie();
+            boolean isConsistent = true;
 
-            for (int i = 0; i < N; i++) {
-                phone_numbers[i] = br.readLine();
+            for (int i = 0; i < n; i++) {
+                phoneNumbers.add(br.readLine());
             }
-            Arrays.sort(phone_numbers);
 
-            if (isConsistent(N, phone_numbers)) {
+            for (String number : phoneNumbers) {
+                if (trie.insert(number)) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+
+            if (isConsistent) {
                 sb.append("YES\n");
-            } else {
+            }
+            else {
                 sb.append("NO\n");
             }
         }
-
         System.out.println(sb);
     }
-
-    public static boolean isConsistent(int N, String[] phone_number) {
-        // 오름차순 정렬된 상태에서 현재 문자열이 다음 문자열의 접두어인지 확인
-        for (int i = 0; i < N - 1; i++) {
-            if (phone_number[i + 1].startsWith(phone_number[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
