4b2ec98da557b143c756340b2f4d2f2ebfc08692
조세영 <93782168+ilu25@users.noreply.github.com>
2025-04-23 12:18:34 +0900

    [Silver I] Title: 스타트링크, Time: 160 ms, Memory: 64612 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -2,21 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int F, S, G, U, D;
-	static int min = -1;
-	static boolean[] visited;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
-		F = Integer.parseInt(input[0]);
-		S = Integer.parseInt(input[1]);
-		G = Integer.parseInt(input[2]);
-		U = Integer.parseInt(input[3]);
-		D = Integer.parseInt(input[4]);
-		visited = new boolean[F + 1];	// 1 ~ F층
+		int F = Integer.parseInt(input[0]);
+		int S = Integer.parseInt(input[1]);
+		int G = Integer.parseInt(input[2]);
+		int U = Integer.parseInt(input[3]);
+		int D = Integer.parseInt(input[4]);
+		boolean[] visited = new boolean[F + 1];	// 1 ~ F층
 		
-		BFS();
+		Queue<int[]> stairs = new LinkedList<>();
+		stairs.offer(new int[] {S, 0});
+		visited[S] = true;
+		int min = -1;
+		
+		while (!stairs.isEmpty()) {
+			int[] stair = stairs.poll();
+			
+			if (stair[0] == G) {
+				min = stair[1];
+				break;
+			}
+			
+			// 업 & 다운
+			int[] next = new int[] {stair[0] + U, stair[0] - D};
+			if (U != 0 && next[0] <= F) {
+				if (!visited[next[0]]) {
+					visited[next[0]] = true;
+					stairs.offer(new int[] {next[0], stair[1] + 1});
+				}
+			}
+			if (D != 0 && next[1] >= 1) {
+				if (!visited[next[1]]) {
+					visited[next[1]] = true;
+					stairs.offer(new int[] {next[1], stair[1] + 1});
+				}
+			}
+		}
 		
 		StringBuilder sb = new StringBuilder();
 		if (min != -1) {
@@ -27,45 +50,15 @@ public class Main {
 		}
 		System.out.print(sb);
 	}
-	
-	static void BFS() {
-		Queue<Integer> stairs = new LinkedList<>();
-		stairs.offer(S);
-		int cnt = 0;
-		
-		while (!stairs.isEmpty()) {
-			// 업 & 다운해서 도착한 층들 확인 -> 횟수 증가
-			int size = stairs.size();
-			while (size > 0) {
-				size--;
-				int stair = stairs.poll();
-				
-				if (stair == G) {
-					min = cnt;
-					return;
-				}
-				
-				// 업 & 다운
-				if (stair < 1 || stair > F) continue;
-				if (visited[stair]) continue;
-				
-				visited[stair] = true;
-				
-				stairs.offer(stair + U);
-				stairs.offer(stair - D);
-			}
-			cnt++;
-		}
-	}
 }
 
 /* 
- * 11:11 ~ 11:47
+ * 11:11 ~ 12:18
  * - BFS로 G층의 최소 버튼 수 구하기
  * - U 또는 D가 0인 경우 주의하기
  * - visited로 부하 감소
  * 
  * [과정]
- * - 메모리 초과
- * -> 정답 풀이 참고 (큐 사이즈만큼 확인 후 횟수 증가시키기)
+ * - ~34: 메모리 초과
+ * -> visited 업데이트를 안 함..
  */
