71f3c0f3d272ab5557f4896af3356bfe264589e8
조세영 <93782168+ilu25@users.noreply.github.com>
2025-04-25 16:35:41 +0900

    [level 2] Title: 타겟 넘버, Time: 5.10 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static vector<int> nums;
+static int t;
+static int answer = 0;
+
+void dfs(int idx, int temp) {
+    if (idx == nums.size()) {
+        if (temp == t) {
+            answer++;
+        }
+        return;
+    }
+    
+    dfs(idx + 1, temp + nums[idx]);
+    dfs(idx + 1, temp - nums[idx]);
+}
+
+int solution(vector<int> numbers, int target) {
+    nums = numbers;
+    t = target;
+    
+    dfs(0, 0);
+    
+    return answer;
+}
+
+/*
+    3:46 ~ 
+    - n개: 음이 아닌 정수
+    - 순서 바꾸지 않고 더하기/빼기로 타겟 넘버 만들기
+
+    [풀이]
+    - DFS: "signal: segmentation fault (core dumped)" -> idx 넘어갈 때 무조건 return
+    - BFS: 시간 초과
+*/
